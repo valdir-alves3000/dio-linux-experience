85444fcdbe0923258fca0856623616ad801c8aca
valdir-alves3000 <valdiralves3000@gmail.com>
2022-08-21 11:28:32 -0300

    Update Apache Start

--- iac2.sh
+++ iac2.sh
@@ -7,12 +7,21 @@ apt-get update -y
 apt-get install apache2 -y
 apt-get install unzip -y
 
+echo "Ativando o servidor Apache"
+
+sudo service apache2 start
+
 echo "Baixando e copiando os arquivos da aplicação..."
 
 cd /tmp
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/h>unzip main.zip
+rm -rf main.zi**
+
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+
+
 cd linux-site-dio-main
-cp -R * var/www/html/
+cp -R * /var/www/html/
 
 
 echo "FIM"
